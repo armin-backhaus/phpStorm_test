fbdfc9a1444b1d3b2a2f72b17e310aff9574a6f2
armin-backhaus <arminbackhaus43@gmail.com>
2025-08-12 13:20:02 +0200

    WIP First function

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,33 +1,24 @@
 <?php
-// repo_fallen-phenix
 // Zipp on 7
-
-$result = (int)(4 / 3);
-echo "result: " . $result . "<br>";
-
-$result2 = 9;
-if ($result2 % 3 == 0) {
-    $myBool = "true";
-} else {
-    $myBool = "false";
-}
-echo "result2: " . $myBool . "<br>";
-
-echo("Hello World!");
 
 // loop from 1-100 php mit "echo"!
 for ($i = 1; $i <= 100; $i++) {
 
     if ($i % 3 == 0 && $i % 5 == 0) {
-        echo "FizzBuzz" . "<br>";
+        output("FizzBuzz");
     } elseif ($i % 5 == 0) {
-        echo "Buzz" . "<br>";
+        output("Buzz");
     } elseif ($i % 3 == 0) {
-        echo "Fizz" . "<br>";
+        output("Fizz");
     } else {
-        echo("test \"$i\" <br /> \n");
+        output($i);
     }
 
+}
+
+function output($test)
+{
+    echo $test . "<br><br>";
 }
 
 // todo ascii tabelle
@@ -36,5 +27,3 @@ for ($i = 1; $i <= 100; $i++) {
 
 // var_dump(5 / 3);
 // buzz on 5
-$expressions = "GoSlowBecomeFast";
-echo($expressions);
