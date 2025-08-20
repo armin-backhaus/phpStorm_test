48472ffbdd49444d9c4d5317bd1765d2731bc7e2
armin-backhaus <arminbackhaus43@gmail.com>
2025-08-20 13:40:40 +0200

    WIP Fizz and Buzz

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,7 +1,13 @@
 <?php
 
-for ($i = 1; $i <= 1337; $i++) {
-    echo("$i<br><br>");
-    if ($i % 3 == 0)
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 == 0) {
         echo("Fizz") . "<br><br>";
+    } elseif ($i % 5 == 0) {
+        echo("Buzz") . "<br><br>";
+    } else {
+        echo("$i<br><br>");
+    }
 }
+
+// erweitern das FizzBuzz bei 3 und 5 ohne rest ausgegeben wird
