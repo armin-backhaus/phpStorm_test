1413c27bfe248a5876650f578945610c13262193
armin-backhaus <arminbackhaus43@gmail.com>
2025-08-20 13:48:41 +0200

    WIP Fizz and Buzz

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,6 +1,9 @@
 <?php
 
 for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 ==0 && $i % 5 ==0) {
+        echo("FizzBuzz");
+    }
     if ($i % 3 == 0) {
         echo("Fizz") . "<br><br>";
     } elseif ($i % 5 == 0) {
