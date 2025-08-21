196798481891a5b1c56a78e0e1ab372bcd527985
armin-backhaus <arminbackhaus43@gmail.com>
2025-08-21 12:21:26 +0200

    WIP if to elseif on line 8

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,14 +1,11 @@
 <?php
 
-for ($i = 1; $i <= 100; $i++)
-{
+for ($i = 1; $i <= 100; $i++) {
     // erweitern das FizzBuzz bei 3 und 5 ohne rest ausgegeben wird
 
     if ($i % 3 == 0 && $i % 5 == 0) {
         echo("FizzBuzz");
-    }
-
-    if ($i % 3 == 0) {
+    } elseif ($i % 3 == 0) {
         echo("Fizz <br><br>");
     } elseif ($i % 5 == 0) {
         echo("Buzz <br><br>");
