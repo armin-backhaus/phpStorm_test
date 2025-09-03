d6d5e5ce1c01a7c3e7ad7e11938f6eac70a58113
armin-backhaus <arminbackhaus43@gmail.com>
2025-09-03 12:01:31 +0200

    ADD gotta chach em all 👾

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -2,18 +2,20 @@
 // if bedingung zu FizzBuzzZippZapp erweitern ! dont repeat yourself!
 // wie kann ich bei Zipp einen 9 anstelle der 11 fehlerfrei nutzen?
 
-for ($i = 1; $i <= 2000; $i++) {
+for ($i = 1; $i <= 100; $i++) {
 
-    if ($i % 3 == 0 && $i % 5 == 0 && $i % 7 == 0 && $i % 11 == 0) {
-        echo("FizzBuzzZippZapp<br><br>");
+    if ($i % 3 == 0 && $i % 5 == 0 && $i % 7 == 0) {
+        echo("FizzBuzzZipp<br><br>");
+    } elseif ($i % 5 == 0 && $i % 7 == 0) {
+        echo("BuzzZipp <br><br>");
+    } elseif ($i % 3 == 0 && $i % 7 == 0) {
+        echo("FizzZipp <br><br>");
     } elseif ($i % 3 == 0) {
         echo("Fizz <br><br>");
     } elseif ($i % 5 == 0) {
         echo("Buzz <br><br>");
     } elseif ($i % 7 == 0) {
         echo("Zipp <br><br>");
-    } elseif ($i % 11 == 0) {
-        echo("Zapp <br><br>");
     } else {
         echo("$i<br><br>");
     }
