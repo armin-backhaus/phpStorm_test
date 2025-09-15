2a935bd8a1b80054471adae470b9ac4e5dca89ce
armin-backhaus <arminbackhaus43@gmail.com>
2025-09-15 12:05:15 +0200

    ADD functions to class in Output.php

--- src/Output.php
+++ src/Output.php
@@ -7,8 +7,44 @@ class Output
         echo "$something <br><br>";
     }
 
-    function zipp()
+    function divisibleByThreeAndFiveAndSeven($something)
     {
-        echo "Zipp <br><br>";
+        echo "$something <br><br>";
     }
+
+    function divisibleByFiveAndSeven($something)
+    {
+        echo "$something <br><br>";
+    }
+
+    function divisibleByThreeAndFive($something)
+    {
+        echo "$something <br><br>";
+    }
+
+    function divisibleByThreeAndSeven($something)
+    {
+        echo "$something <br><br>";
+    }
+
+    function divisibleByThree($something)
+    {
+        echo "$something <br><br>";
+    }
+
+    function divisibleByFive($something)
+    {
+        echo "$something <br><br>";
+    }
+
+    function divisibleBySeven($something)
+    {
+        echo "$something <br><br>";
+    }
+
+
+function zipp()
+{
+    echo "Zipp <br><br>";
+}
 }
