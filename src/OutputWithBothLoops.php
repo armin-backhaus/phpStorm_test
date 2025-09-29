883150afbfd8b52af06833daf78d7ef5d9efcd89
armin-backhaus <arminbackhaus43@gmail.com>
2025-09-29 13:31:45 +0200

    ADD OutputWithBothLoops.php

--- src/OutputWithBothLoops.php
+++ src/OutputWithBothLoops.php
@@ -6,6 +6,6 @@ class OutputWithBothLoops implements OutputInterface
 {
     function output($something, $i = 0)
     {
-        echo $something, $i . "<br />";
+        echo $something . ' - ', $i . "<br />";
     }
 }
