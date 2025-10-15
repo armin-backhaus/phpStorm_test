906b9fd4cfe620530a388a443d6220e39baaab79
armin-backhaus <arminbackhaus43@gmail.com>
2025-10-15 12:49:52 +0200

    ADD another readably variant

--- index.php
+++ index.php
@@ -47,13 +47,49 @@ echo(11);
       include('./src/OutputWithColor.php');
       //$fizzBuzz = new FizzBuzz(13);
       //$fizzBuzz->run();
+
       (new FizzBuzz(1,7, new OutputWithLineNumbers()))->run();
+
+      $outputObject = new OutputWithLinenumbers();
+      $fizzBuzz1 = new FizzBuzz(1,7, $outputObject);
+      $fizzBuzz1->run();
+
       (new FizzBuzz(3,9, new OutputWithLinebreak()))->run();
+
+      $outputObject = new OutputWithLinebreak();
+      $fizzBuzz2 = new FizzBuzz(3,9, $outputObject);
+      $fizzBuzz2->run();
+
       (new FizzBuzz(3,9, new OutputWithDoubleNumbers()))->run();
+
+      $outputObject = new OutputWithDoubleNumbers();
+      $fizzBuzz3 = new FizzBuzz(3,9, $outputObject);
+      $fizzBuzz3->run();
+
       (new FizzBuzz(9,3, new OutputWithLineNumbers()))->run();
+
+      $outputObject = new OutputWithLinenumbers();
+      $fizzBuzz4 = new FizzBuzz(3,9, $outputObject);
+      $fizzBuzz4->run();
+
       (new FizzBuzz(1,9, new OutputWithEmote()))->run();
+
+      $outputObject = new OutputWithEmote();
+      $fizzBuzz5 = new FizzBuzz(1,9, $outputObject);
+      $fizzBuzz5->run();
+
       (new FizzBuzz(1,9, new OutputWithColor()))->run();
+
+      $outputObject = new OutputWithEmote();
+      $fizzBuzz6 = new FizzBuzz(1,9, $outputObject);
+      $fizzBuzz6->run();
+
       (new FizzBuzz(1,9, new OutputWithBothLoops()))->run();
+
+      $outputObject = new OutputWithBothLoops();
+      $fizzBuzz7 = new FizzBuzz(1,9, $outputObject);
+      $fizzBuzz7->run();
+
       //(new FizzBuzz(1,9, new OutputWithDoubleNumbers()))->genericLoop();
       ?>
 
