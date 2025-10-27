487cf1d4b403756fee52beeef1f160e9c4c48327
armin-backhaus <arminbackhaus43@gmail.com>
2025-10-27 12:03:37 +0100

    ADD my first test ✨💖✨

---
+++ test/DivisibleCheck/CheckThreeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DivisibleCheck;
+
+use PHPUnit\Framework\TestCase;
+
+class CheckThreeTest extends TestCase
+{
+    public function testCheckWorks(): void
+    {
+        $this->assertTrue(true);
+    }
+}
