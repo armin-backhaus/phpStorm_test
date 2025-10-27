ae489c441928d147d89104b305b4a54e1d14662c
armin-backhaus <arminbackhaus43@gmail.com>
2025-10-27 12:46:32 +0100

    RFC my first real test 💪

--- test/DivisibleCheck/CheckThreeTest.php
+++ test/DivisibleCheck/CheckThreeTest.php
@@ -6,8 +6,35 @@ use PHPUnit\Framework\TestCase;
 
 class CheckThreeTest extends TestCase
 {
-    public function testCheckWorks(): void
+    private $dut;
+
+    public function setUp(): void
     {
-        $this->assertTrue(true);
+        $this->dut = new CheckThree();
+    }
+    protected function tearDown(): void
+    {
+        //clean up
+    }
+
+    final public function testIsBool(): void
+    {
+        $result = $this->dut->check(9);
+
+        $this->assertIsBool($result);
+    }
+
+    final public function testIsDivisible(): void
+    {
+        $result = $this->dut->check(3);
+
+        $this->assertTrue($result);
+    }
+
+    final public function testIsNotDivisible(): void
+    {
+        $result = $this->dut->check(4);
+
+        $this->assertFalse($result);
     }
 }
