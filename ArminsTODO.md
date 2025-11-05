1165ccb63e54e2bb62a5c2ea55c958b22281e633
armin-backhaus <arminbackhaus43@gmail.com>
2025-11-05 12:16:40 +0100

    WIP EqualAndIdenticalTest.php

--- ArminsTODO.md
+++ ArminsTODO.md
@@ -2,5 +2,7 @@
 - SpaceSeparatedTest with Dark Iron Dwarf and Dungeon Master ✔️
 - find user stories for battle concept
 - cant find any user story so far...wip...
+- 
 - create learning test for == and ===
+- https://www.php.net/manual/en/language.operators.comparison.php
 - 
--- test/Learning/EqualAndIdenticalTest.php
+++ test/Learning/EqualAndIdenticalTest.php
@@ -2,9 +2,20 @@
 
 namespace Learning;
 
+use Divisible\DivisibleBy;
 use PHPUnit\Framework\TestCase;
 
 class EqualAndIdenticalTest extends TestCase
 {
+    final public function testEqualAndIdentical(): void
+    {
+        $a = 1;
+        $b = 2;
 
+        $this->assertTrue($a == $b);
+        $this->assertTrue($a === $b);
+
+        $this->assertFalse($a == $b);
+        $this->assertFalse($a === $b);
+    }
 }
