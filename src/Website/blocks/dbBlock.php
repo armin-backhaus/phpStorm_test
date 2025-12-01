7328fec57676d170e92f69b05d825b05e0c54191
apb <arminbackhaus43@gmail.com>
2025-12-01 13:42:39 +0100

    ADD my first query and Hannes joins the database party

--- src/Website/blocks/dbBlock.php
+++ src/Website/blocks/dbBlock.php
@@ -1,22 +1,33 @@
 <?php
 
-
+// https://de.wikipedia.org/wiki/SQL example to build
 
 $mysqli = new mysqli("localhost", "root", "Password", "wow");
 
 if ($mysqli->connect_error) {
     die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
 }
+$name = "Hannes";
+
+$stmt = $mysqli->prepare("INSERT INTO wow_test (name) VALUES (?)");
+$stmt->bind_param("s", $name);
+
+if ($stmt->execute()) {
+    echo "Eintrag erfolgreich hinzugefügt!";
+} else {
+    echo "Fehler beim Hinzufügen des Eintrags: " . $stmt->error;
+}
 
 $sql = "SELECT * FROM wow_test";
 $result = $mysqli->query($sql);
+var_dump($result);
 
-echo "bla";
 while ($row = $result->fetch_assoc()) {
+    var_dump($row);
     echo $row["name"] . "<br>";
 }
 
-
 $mysqli->close();
+$stmt->close();
 
 return ["dbBlock"];
