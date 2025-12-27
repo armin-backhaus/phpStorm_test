89db40f140c501a3fecf4150dcbfe6584903bcdf
apb <arminbackhaus43@gmail.com>
2025-12-27 13:17:41 +0100

    WIP [>_] and button

---
+++ src/Website/blocks/Formular.php
@@ -0,0 +1,22 @@
+<?php
+
+//<body>
+function formular()
+{
+    <form action="" method="POST">
+        <input type="text" name="mein_text" placeholder="Schreib etwas..." required>
+<button type="submit" name"absenden">Anzeigen</button>
+<form>
+}
+
+function sendungsPrüfung()
+{
+    if (isset($_POST['absenden'])) {
+        $eingabe = htmlspecialchars($_POST['mein_text']);
+
+        echo "<h3>Deine Eingabe:</h3>";
+        echo "<p>" . $eingabe . "</p>";
+    }
+}
+//</body>
+//</html>
